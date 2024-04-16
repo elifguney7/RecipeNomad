d08251d509f220f46fe75432503122e5f872167c
elifguney7 <elifguney13@gmail.com>
2024-04-17 00:43:01 +0300

    signin signup css

--- recipenomad_ng/src/app/components/sign-up/sign-up.component.ts
+++ recipenomad_ng/src/app/components/sign-up/sign-up.component.ts
@@ -1,41 +1,6 @@
-// import { Component, OnInit } from '@angular/core';
-// import { UserService } from 'src/app/services/user.service';
-
-// @Component({
-//   selector: 'app-sign-up',
-//   templateUrl: './sign-up.component.html',
-//   styleUrls: ['./sign-up.component.css']
-// })
-// export class SignUpComponent implements OnInit{
-//   constructor(private userService: UserService) { }
-//   users: any[] = [];
- 
-//   ngOnInit(): void {
-//     this.userService.getUsers().subscribe(
-//       (data) => {
-//         this.users = data;
-//       },
-//       (error) => {
-//         console.error('Error fetching users:', error);
-//       }
-//     );
-//   }
-
-//   createUser() {
-//     const user = { username: 'John Doe', email: 'john@example.com', age: 30 };
-//     this.userService.createUser(user).subscribe(
-//       response => {
-//         console.log('User created:', response);
-//       },
-//       error => {
-//         console.error('Error creating user:', error);
-//       }
-//     );
-//   }
-// }
-
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -51,13 +16,20 @@ export class SignUpComponent {
     password: ''
   };
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   signUp(form: NgForm) {
     if (form.valid) {
       this.userService.createUser(this.user).subscribe({
-        next: (response) => console.log('User created', response),
-        error: (error) => console.error('Error creating user', error)
+        next: (response) => {
+          console.log('User created', response);
+          // Redirect to the sign-in page after successful sign-up
+          this.router.navigate(['/sign-in']);
+        },
+        error: (error) => {
+          console.error('Error creating user', error);
+          // Optionally handle errors here (e.g., display an error message)
+        }
       });
     }
   }
