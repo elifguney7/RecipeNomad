8aa9662a5ceacfff23ac96033d6ed0c029fd0d87
elifguney7 <elifguney13@gmail.com>
2024-04-16 22:02:24 +0300

    profile data from db

--- recipenomad_ng/src/app/components/profile/profile.component.ts
+++ recipenomad_ng/src/app/components/profile/profile.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  user: any = {};
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getUserProfile().subscribe({
+      next: (data) => {
+        this.user = data;
+      },
+      error: (error) => {
+        console.error('Failed to load user data', error);
+      }
+    });
+  }
 }
--- recipenomad_ng/src/app/services/user.service.ts
+++ recipenomad_ng/src/app/services/user.service.ts
@@ -24,9 +24,10 @@
 //   }
 // }
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators'; // Import the tap operator
 
 @Injectable({
   providedIn: 'root'
@@ -41,9 +42,23 @@ export class UserService {
   }
 
   loginUser(credentials: { email: string; password: string }): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/login`, credentials);
+    return this.http.post<any>(`${this.apiUrl}/login`, credentials).pipe(
+      tap((resp) => {
+        localStorage.setItem('token', resp.token); // Assuming the token is returned directly
+      })
+    );
   }
+  
+  getUserProfile(): Observable<any> {
+    // Retrieve token from localStorage or your preferred storage method
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
 
+    return this.http.get<any>(`${this.apiUrl}/profile`, { headers: headers });
+  }
+  
   updateUser(userId: string, user: any): Observable<any> {
     return this.http.put<any>(`${this.apiUrl}/${userId}`, user);
   }
